2bbbe7c5d903ceafd40a4553ee7b160e0256de51
Raj Tangadi <rajtangadi65@gmail.com>
2025-04-04 22:33:16 +0530

    implement login, register and logout api

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.router.js";
 dotenv.config();
 const app = express();
 import connectDB from "./db/connectDB.js";
@@ -10,6 +12,9 @@ const PORT = process.env.PORT || 3000;
 //middleware
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+app.use("/api/auth", authRouter);
+
 
 app.listen(PORT, () => {
   connectDB();
